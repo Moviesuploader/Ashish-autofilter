ff7ff3f4ade2d573b7b0edafe0c67d8225005d68
Moviesuploader <138097021+Moviesuploader@users.noreply.github.com>
2025-03-01 21:15:37 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -280,7 +280,7 @@ async def send_msg(bot, filename, caption):
 
         filename = re.sub(r"[\(\)\[\]\{\}:;'\-!]", "", filename)
 
-        text = "#𝑵𝒆𝒘_𝑭𝒊𝒍𝒆_𝑨𝒅𝒅𝒆𝒅 ✅\n\n🏷️𝑵𝒂𝒎𝒆: `{}`\n\n<b>🔮𝑸𝒖𝒂𝒍𝒊𝒕𝒚: {}\n\n🔊𝑨𝒖𝒅𝒊𝒐: {}</blockquote></b>"
+        text = "#𝑵𝒆𝒘_𝑭𝒊𝒍𝒆_𝑨𝒅𝒅𝒆𝒅 ✅\n\n<b>🏷️𝑵𝒂𝒎𝒆: `{}`\n\n<blockquote>🔮𝑸𝒖𝒂𝒍𝒊𝒕𝒚: {}\n\n🔊𝑨𝒖𝒅𝒊𝒐: {}</blockquote></b>"
         text = text.format(filename, quality, language)
 
         if await add_name(OWNERID, filename):
